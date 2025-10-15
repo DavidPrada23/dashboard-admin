92f54af61ae5d5ba98f9b71634445725bb29f8e3
DavidPrada23 <davidprada23@gmail.com>
2025-10-15 10:50:47 -0500

    se ajusta el endpoint para solucionar el problema 500 en el response

--- src/components/LlaveForm.tsx
+++ src/components/LlaveForm.tsx
@@ -20,7 +20,7 @@ export default function LlaveForm({ modoRegistroInicial = false, onConfigSaved }
     try {
       const token = localStorage.getItem("token");
       await axios.post(
-        "api/llaves",
+        "llaves",
         { valor: llave, emailBancario },
         { headers: { Authorization: `Bearer ${token}` } }
       );
